7f250941cfffd7972ff637f92153f8c56025c431
Miguel Angel -Cheroki- <miguelangelmorato84@gmail.com>
2020-10-23 12:30:32 +0200

    added button logout to profile

--- src/containers/Profile/Profile.jsx
+++ src/containers/Profile/Profile.jsx
@@ -1,15 +1,29 @@
 import React from 'react';
-import { Link, Redirect } from 'react-router-dom';
+import { Link, Redirect, BrowserRouter, useHistory } from 'react-router-dom';
 
 
 const Profile = () => {
+    const history = useHistory();
+
     const validator = localStorage.getItem('token');
     if (!validator) return <Redirect to='/' />
+
+    const salir = () => {
+        localStorage.clear();
+        //localStorage.removeItem('token');
+        history.push('/');
+    }
+
     return (
-        <div>
-            <Link to="/">citas</Link>
-            <Link to="/signup">Signup</Link>
-        </div>
+        <BrowserRouter>
+            <header>
+                <button onClick={salir}>Salir</button>
+            </header>
+            <div>
+                <Link to="/">citas</Link>
+                <Link to="/signup">Signup</Link>
+            </div>
+        </BrowserRouter>
     )
 }
 
