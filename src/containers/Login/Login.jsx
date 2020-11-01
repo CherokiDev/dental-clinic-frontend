b55178997b99a875a2b1baa9b0770609210dd350
Miguel Angel -Cheroki- <miguelangelmorato84@gmail.com>
2020-11-01 09:51:32 +0100

    improved security and information for the user in Login

--- src/containers/Login/Login.jsx
+++ src/containers/Login/Login.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
 import { Link, useHistory } from 'react-router-dom';
 import './Login.scss';
@@ -7,6 +7,7 @@ import './Login.scss';
 const Login = () => {
 
 	const history = useHistory();
+	const [mensajeError, setMensajeError] = useState();
 
 	const handleSubmit = event => {
 		event.preventDefault();
@@ -20,6 +21,7 @@ const Login = () => {
 				localStorage.setItem("token", JSON.stringify(res.data.token))
 				localStorage.setItem("user", JSON.stringify(res.data))
 				console.log(localStorage.getItem('token'));
+				
 
 
 				setTimeout(() => {
@@ -27,26 +29,33 @@ const Login = () => {
 				}, 1500);
 
 			})
-			.catch(error => console.log(error.response.data))
+			.catch(error => {
+				//console.log(error.response.data);
+				setMensajeError(error.response.data.message)
+			})
+			
 	}
 
 	return (
 		<body className="body">
 
-      <div className="header">
-        <div className="buttons">
-				  <Link to='/'>Back</Link>
-        </div>
-      </div>
+			<div className="header">
+				<div className="buttons">
+					<Link to='/'>Back</Link>
+				</div>
+			</div>
 
 			<div className="containerFormLogin">
 				<form className="loginForm" onSubmit={handleSubmit}>
-					<input className="formEmail" type="email" name="email" required placeholder=" Introduce tu email" />
-					<input type="password" name="password" required placeholder=" Introduce tu contraseña" />
+					<div className="textForm">Email:</div>
+					<input className="formEmail" type="email" name="email" required placeholder="Please enter your email" />
+					<div className="textForm">Password:</div>
+					<input type="password" name="password" required placeholder="Please enter your password" />
+					<div className="mensajeError">{mensajeError}</div>
 					<button type="submit">Log in</button>
 				</form>
-      </div>
-      
+			</div>
+
 		</body>
 	)
 }
